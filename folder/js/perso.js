e3496d57ece68b3aa8c3f93dd42a498fd9451f56
unknown <Bryan@Bryan-VAIO.(none)>
2014-01-27 01:43:07 +0100

    Commit du dossier comprenant le structure de base du site

---
+++ folder/js/perso.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	var btnOpenVolet = document.getElementById('btnOpenVolet');
+	var btnCloseVolet = document.getElementById('btnCloseVolet');
+	var timerAnim = 0.5;
+
+	$('#btnOpenVolet').click(function(){
+		if( document.body.clientWidth < 768 ){
+			TweenMax.to("#voletAction", timerAnim, {height:"100%"});
+		}else{
+			TweenMax.to("#voletAction", timerAnim, {height:"300px"});
+		}
+		btnOpenVolet.style.display = "none";
+		btnCloseVolet.style.display = "block";
+	return false;  
+	});
+	$('#btnCloseVolet').click(function(){
+		TweenMax.to("#voletAction", timerAnim, {height:50});
+		btnCloseVolet.style.display = "none";
+		btnOpenVolet.style.display = "block";	
+	return false;  
+	});
+	
+});
