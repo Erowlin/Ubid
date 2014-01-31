956c9b950111a5c774f7e1477b21340246ce8659
Benedick steve <benedicksteve@gmail.com>
2014-01-31 12:58:21 -0500

    Modification barre action vue espace

--- folder/js/espace.js
+++ folder/js/espace.js
@@ -1,3 +1,34 @@
-/**
- * Created by steve on 30/01/14.
- */
+$(document).ready(function(){
+
+    var btnOpenVoletEspacePerso = document.getElementById('btnOpenVoletEspacePerso');
+    var btnCloseVoletEspacePerso = document.getElementById('btnCloseVoletEspacePerso');
+    var espacePerso = document.getElementById('rowEspacePerso');
+    var timerAnim = 0.5;
+
+    $('#btnOpenVoletEspacePerso').click(function(){
+        if( document.body.clientWidth < 768 ){
+            TweenMax.to("#voletAction", timerAnim, {height:"100%"});
+        }else{
+            TweenMax.to("#voletAction", timerAnim, {height:"300px"});
+        }
+        btnOpenVoletEspacePerso.style.display = "none";
+        btnCloseVoletEspacePerso.style.display = "block";
+        return false;
+    });
+    $('#btnCloseVoletEspacePerso').click(function(){
+        TweenMax.to("#voletAction", timerAnim, {height:"50px"});
+        btnCloseVoletEspacePerso.style.display = "none";
+        btnOpenVoletEspacePerso.style.display = "block";
+        return false;
+    });
+    $('.rowEspacePerso').click(function(){
+        if( document.body.clientWidth < 768 ){
+            TweenMax.to("#voletAction", timerAnim, {height:"100%"});
+        }else{
+            TweenMax.to("#voletAction", timerAnim, {height:"300px"});
+        }
+        btnOpenVoletEspacePerso.style.display = "none";
+        btnCloseVoletEspacePerso.style.display = "block";
+        return false;
+    });
+});
