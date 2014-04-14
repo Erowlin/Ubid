10abf8c4d2084c1076e60a01aa2f78abdded706b
Thomas Romera <erowlin@Thomass-MacBook-Air.local>
2014-04-14 05:20:47 -0400

    Ajout de commentaires

--- flask/new/helpers.py
+++ flask/new/helpers.py
@@ -1,3 +1,4 @@
+# Permet de transformer n'importe quelle requête en entrée vers un format unique. Permet d'avoir un format standard, que ce soit du POST, PUT, DELETE, GET, et depuis ANgular ou Postman.
 def get_response(request):
 	if len(request.form) > 0: # If POST from WEb Browser
         		resp = request.form
@@ -7,6 +8,8 @@ def get_response(request):
 		resp = request.args # If GET Values
     	return resp
 
+
+# Permet de transformer une liste de Model en json. Sérialize chaque objet dans la liste en Json, puis retourne une nouvelle liste au format Json
 def list_to_json(liste):
 	json = []
 	for elem in liste:
